3c73afdc6a13922a99ee75330123d1aa2dfb5522
Daphne Zwuup <69635977+DphnZwp@users.noreply.github.com>
2023-06-28 21:38:51 +0200

    basic setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+function Footer() {
+	return (
+		<>
+			<footer>
+				<h2>fdsdfsds</h2>
+				<p>
+					FDND maakt gebruik van een heleboel 
+					microsites in plaats van één grote website 
+					met een ingewikkelde informatiestructuur. Microsites zijn kleine websites 
+					met een specifiek doel ter ondersteuning van een campagne, informatie 
+					over een product of dienst.
+				</p>
+				<ul>
+					<li>FDND.nl</li>
+					<li>Contact en routebeschrijving</li>
+					<li>Programma</li>
+					<li>Studie Keuze Check</li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</footer>
+		</>
+	 );
+}
+
+export default Footer;
---
+++ src/components/QuizCard.tsx
@@ -0,0 +1,26 @@
+interface QuizCardProps {
+  name: string;
+	description: string;
+  category: string;
+	answertype: string;
+	link: string;
+	image: string;
+	alt: string;
+}
+
+function QuizCard({name, description, category, answertype, link, image, alt}: QuizCardProps) {
+  return (
+		<>
+			<article>
+				<h2>{name}</h2>
+				<p>{description}</p>
+				<p><strong>Categorie:</strong> {category}</p>
+				<p><strong>Antwoordtype:</strong> {answertype}</p>
+				<a href={link} >Speel de quiz</a>
+			</article>
+			<img src={image} alt={alt} />
+		</>
+  );
+}
+
+export default QuizCard;
