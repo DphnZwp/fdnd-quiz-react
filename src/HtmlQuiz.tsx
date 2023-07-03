9d589d7c4c0e4f96883d302188df226cb4091867
DphnZwp <zwuupdaphne@gmail.com>
2023-07-03 23:51:00 +0200

    trying to solve error

--- src/HtmlQuiz.tsx
+++ src/HtmlQuiz.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import HtmlData from './HtmlData.json';
 
 function HtmlQuiz() {
